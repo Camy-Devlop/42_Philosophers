6d309f0c85679a190254f858a347afb34290a231
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2025-04-02 22:00:05 +0200

    Create philosophers.h
    creation of file header with the lib

---
+++ include/philosophers.h
@@ -0,0 +1,11 @@
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
