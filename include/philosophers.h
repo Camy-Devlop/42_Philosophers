399c8b1cb7b72aa1f34180da2cd7b5b35811a5de
Camy-Devlop <adbaibi.ismail@me.com>
2025-04-03 13:47:11 +0200

    test de gettimeofday plus add in de header status

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 22:46:47 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/02 22:46:57 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:40:09 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@
 # include <sys/time.h>
 # include <unistd.h>
 
+# define TAKEN_FORK "taken a fork"
+# define EATING "eating"
+# define SLEEPING "sleeping"
+# define THINKING "thinking"
+# define DEAD "dead"
+
+typedef enum e_status
+{
+	taken_a_fork,
+	eating,
+	sleeping,
+	thinking,
+	dead
+} * t_status;
 #endif
--- src/pilosophers.c
+++ src/pilosophers.c
@@ -6,13 +6,22 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 22:47:30 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/03 00:04:09 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:01:26 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/pilosophers.h"
+#include "../include/philosophers.h"
 
 int main(void)
 {
+	//usleep, gettimeofday
+	//struct timeval *restrict tv,
+        //struct timezone *_Nullable restrict tz
+	struct timeval t;
+//	struct timezone tz;
 
+	printf("retour de time of day = %d\n",gettimeofday(&t,NULL));
+	printf("val sec = %ld\n",t.tv_sec);
+//	printf("val sec = %d\n",tz.tz_minuteswest);
+	return (0);
 }
