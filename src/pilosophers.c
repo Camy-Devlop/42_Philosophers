15f56549dcaf66d212e681f9c0205bb3d308744f
Camy-Devlop <adbaibi.ismail@me.com>
2025-04-18 00:00:21 +0200

    leak disparue super

--- src/pilosophers.c
+++ src/pilosophers.c
@@ -6,7 +6,7 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 22:47:30 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/04/16 15:32:54 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/04/17 23:38:38 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,22 +45,24 @@ void youturn()
 
 int	main(int n, char **tab)
 {
-	struct timeval	t;
-	t_status_philo	s;
+//	struct timeval	t;
+//	t_status_philo	s;
 
 	(void)n;
 	(void)tab;
-	if (n != 6)
-		return (0);
-	printf("retour de time of day = %d\n", gettimeofday(&t, NULL));
-	printf("val sec = %ld\n", t.tv_sec);
-	s = calloc(1, sizeof(enum e_status));
-	s[0] = eating;
-	ft_message_status(s);	
-	free(s);
+//	if (n != 6)
+//		return (0);
+//	printf("retour de time of day = %d\n", gettimeofday(&t, NULL));
+//	printf("val sec = %ld\n", t.tv_sec);
+//	s = calloc(1, sizeof(enum e_status));
+//	s[0] = eating;
+//	ft_message_status(s);	
+//	free(s);
 	pthread_t test;
-	pthread_create(&test,NULL, myturn,NULL);
+//	test = NULL;
+	pthread_create(&test, NULL, myturn, NULL);
 	youturn();
-	free(&test);
+	pthread_join(test ,NULL);
+//	free(test);
 	return (0);
 }
